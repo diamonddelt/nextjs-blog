8601a60e02fa26cfcbfa54ca3fa9086f93cbd9f7
diamonddelt <rrrasti@yahoo.com>
2018-12-24 06:59:08 -0600

    Add basic querystring routing via nextjs router

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
 import Layout from '../components/Layout'
+import Link from 'next/link'
+
+const BlogPostLink = (props) => (
+    <li>
+        <Link href={`/blogpost?title=${props.title}`}>
+            <a>{props.title}</a>
+        </Link>
+    </li>
+)
 
 const Index = () => (
     <Layout>
         <p>
             <h2>Ryan Rasti</h2>
             Github: diamonddelt
+
+            <h3>Posts</h3>
+            <ul>
+                <BlogPostLink title="Exploring Existential Ennui"/>
+                <BlogPostLink title="What is Next.js and why should I care?"/>
+            </ul>
         </p>
     </Layout>
 )
